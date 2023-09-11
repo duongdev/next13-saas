71e81315191f81cb8199ef743433444babb1bd0a
Dustin Do <dustin.do95@gmail.com>
2023-09-12 01:00:01 +0700

    Add email auth with template

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,34 @@
 import NextAuth from 'next-auth'
 import Email from 'next-auth/providers/email'
+import { Resend } from 'resend'
+
+import MagicLinkEmail from '@/components/emails/magic-link'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 const handler = NextAuth({
   providers: [
     Email({
       from: `${process.env.NEXT_PUBLIC_APP_NAME} <noreply@withdustin.com>`,
-      sendVerificationRequest({ identifier: email, url }) {},
+      maxAge: 15 * 60, // 15 minutes
+      async sendVerificationRequest({
+        identifier: email,
+        url,
+        provider: { from },
+      }) {
+        try {
+          await resend.emails.send({
+            from,
+            to: email,
+            subject: 'Đăng nhập vào tài khoản của bạn',
+            react: MagicLinkEmail({ url, expiresIn: '15 phút' }),
+            text: `Đăng nhập vào tài khoản của bạn: ${url}`,
+          })
+        } catch (error) {
+          console.log(error)
+          throw new Error('Không thể gửi email')
+        }
+      },
     }),
   ],
 })
--- src/components/emails/magic-link.tsx
+++ src/components/emails/magic-link.tsx
@@ -1,9 +1,24 @@
 import { type FC } from 'react'
 
-export type MagicLinkEmailProps = {}
+import { Html } from '@react-email/html'
+import { Link } from '@react-email/link'
 
-const MagicLinkEmail: FC<MagicLinkEmailProps> = () => {
-  return <>magic-link</>
+export type MagicLinkEmailProps = {
+  url: string
+  expiresIn: string
+}
+
+const MagicLinkEmail: FC<MagicLinkEmailProps> = ({ url, expiresIn }) => {
+  return (
+    <Html lang="en">
+      <p>
+        Hey, bạn có thể đăng nhập vào tài khoản tại{' '}
+        {process.env.NEXT_PUBLIC_APP_NAME} bằng cách click vào link bên dưới.
+      </p>
+      <Link href={url}>{url}</Link>
+      <p>Link sẽ hết hạn trong {expiresIn}.</p>
+    </Html>
+  )
 }
 
 MagicLinkEmail.displayName = 'MagicLinkEmail'
